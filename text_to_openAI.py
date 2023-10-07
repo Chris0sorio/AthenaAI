eeab4c39f44111d452e562db2ce61e5935bc6bf0
Chris Osorio <chrisosorio1011@gmail.com>
2023-10-07 16:50:14 -0400

    Added print lines for log

--- text_to_openAI.py
+++ text_to_openAI.py
@@ -66,9 +66,10 @@ def send_to_chatGPT(messages, model="gpt-3.5-turbo"):
 messages = [{"role": "user", "content": "Please act like Jarvis from iron man."}]
 while(1):
     text = record_text()
-    print("Wrote text: ", text)
+    print("Wrote text: ", text, "\n")
+    print("Generating response...\n")
     messages.append({"role": "user", "content": text})
     response = send_to_chatGPT(messages)
+    print("Response: ", response, "\n")
+    print("Speaking...\n")
     SpeakText(response)
-
-    print("Response: ", response)
