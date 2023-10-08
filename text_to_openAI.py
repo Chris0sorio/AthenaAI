a89bd18d52081f4f7502808882aacba4a5b6bb9d
Chris Osorio <chrisosorio1011@gmail.com>
2023-10-08 01:52:54 -0400

    Added wake word and kill term

--- text_to_openAI.py
+++ text_to_openAI.py
@@ -1,52 +1,43 @@
 import speech_recognition as sr
 from gtts import gTTS
-
 import os
 from dotenv import load_dotenv
+from pydub import AudioSegment
+from pydub.playback import play
+
 load_dotenv()
 OPENAI_KEY = "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"
 
 import openai
 openai.api_key = OPENAI_KEY
 
-# Function to convert text to speech
-def SpeakText(command):
+# Function to convert text to speech with extended duration
+def SpeakText(command, speed=1.25):
+
     # Create a gTTS object
-    tts = gTTS(text=command)
+    tts = gTTS(text=command, lang='en', slow=False)
+
     # Save the speech to an audio file
     tts.save("output.mp3")
-    # Play the saved audio file
-    #os.system("mpg321 output.mp3")  # For Linux
-    os.system("afplay output.mp3")  # For macOS
-    #os.system("start output.mp3")   # For Windows
+
+    # Load the audio file using pyDub
+    audio = AudioSegment.from_mp3("output.mp3")
+
+    # Adjust the speed of the audio
+    adjusted_audio = audio.speedup(playback_speed=speed)
+
+    # Play the adjusted audio
+    play(adjusted_audio)
+    
 
 # Initialize the recognizer
 rInitializer = sr.Recognizer()
-
-def record_text():
-    # Loop in case of error
-    while True:
-        try: 
-            # Use the microphone as a source for input
-            with sr.Microphone() as source2:
-                # Prepare recognizer to receive input
-                rInitializer.adjust_for_ambient_noise(source2, duration=0.2)
-                print("I'm listening")
-                # Listens for the user's input
-                audio2 = rInitializer.listen(source2)
-                # Using Google to recognize audio
-                MyText = rInitializer.recognize_google(audio2)
-                return MyText
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
-        except sr.UnknownValueError:
-            print("Unknown error occurred")
 
 def send_to_chatGPT(messages, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        max_tokens=100,
+        max_tokens=200,
         n=1,
         stop=None,
         temperature=0.5,
@@ -55,14 +46,50 @@ def send_to_chatGPT(messages, model="gpt-3.5-turbo"):
     messages.append(response.choices[0].message)
     return message
 
-messages = [{"role": "user", "content": "Please act like Jarvis from iron man."}]
+# Function to detect the wake word ("Jarvis")
+def detect_wake_word(audio):
+    try:
+        # Use the recognizer to convert audio to text
+        command = rInitializer.recognize_google(audio)
+        if "Jarvis" in command:
+            return True
+    except sr.UnknownValueError:
+        pass
+    return False
 
-while True:
-    text = record_text()
-    print("Wrote text:", text, "\n")
-    print("Generating response...\n")
-    messages.append({"role": "user", "content": text})
-    response = send_to_chatGPT(messages)
-    print("Response:", response, "\n")
-    print("Speaking...\n")
-    SpeakText(response)
+# Function to check if a kill term is spoken
+def detect_kill_term(audio):
+    try:
+        # Use the recognizer to convert audio to text
+        command = rInitializer.recognize_google(audio)
+        if "thank you" in command:
+            print("Kill term detected. Exiting...")
+            exit()
+    except sr.UnknownValueError:
+        pass
+
+# Initialize messages
+messages = [{"role": "user", "content": "Please act like Jarvis from Iron Man. I am a sir."}]
+
+while(1):
+    
+    with sr.Microphone() as source:
+        print("Listening for 'Jarvis'...")
+        audio = rInitializer.listen(source)
+        
+        if detect_wake_word(audio):
+            print("Wake word detected. Listening for user command...")
+            user_audio = rInitializer.listen(source, timeout=10)  # Listen for user command for up to 10 seconds
+            
+            try:
+                # Use the recognizer to convert audio to text
+                command = rInitializer.recognize_google(user_audio)
+                print("User Command:", command)
+                messages.append({"role": "user", "content": command})
+                response = send_to_chatGPT(messages)
+                print("Response:", response, "\n")
+                print("Speaking...\n")
+                SpeakText(response)
+                detect_kill_term(user_audio)  # Check for the kill term
+            except sr.UnknownValueError:
+                print("Sorry, I didn't catch that.")
